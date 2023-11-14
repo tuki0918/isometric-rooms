596712690a85014171ec0fadf78caba76f405b96
Yuta Yamamoto <tuki0918@gmail.com>
2023-11-15 04:18:01 +0900

    Add hooks/useRooms

---
+++ hooks/useRooms.ts
@@ -0,0 +1,40 @@
+import { useQuery } from "@tanstack/react-query";
+import { client, Queries } from "../utils/microCMS";
+
+export interface ApiResponse {
+  contents: Content[];
+  totalCount: number;
+  offset: number;
+  limit: number;
+}
+
+export interface Content {
+  id: string;
+  createdAt: string;
+  updatedAt: string;
+  publishedAt: string;
+  revisedAt: string;
+  title: string;
+  image: Image;
+  category: string[];
+  is_generated_by_ai: boolean;
+  created_at: string;
+  updated_at: string;
+}
+
+export interface Image {
+  url: string;
+  height: number;
+  width: number;
+}
+
+const fetchRooms = async (queries: Queries) => {
+  return await client.get<ApiResponse>({ endpoint: "rooms", queries: queries });
+};
+
+export const useRooms = (queries: Queries) => {
+  return useQuery({
+    queryKey: ["rooms", queries],
+    queryFn: () => fetchRooms(queries),
+  });
+};
