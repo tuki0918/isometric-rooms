1d4445e341d072cfae32ca9675934b656b37a65d
Yuta Yamamoto <tuki0918@gmail.com>
2023-11-15 04:08:44 +0900

    Add remotePatterns (`images.microcms-assets.io`) to next.config.js

--- next.config.js
+++ next.config.js
@@ -1,4 +1,14 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {}
+const nextConfig = {
+  images: {
+    remotePatterns: [
+        // MicroCMSの画像を対象にする
+        {
+          protocol: 'https',
+          hostname: 'images.microcms-assets.io',
+        },
+      ],
+  },
+};
 
-module.exports = nextConfig
+module.exports = nextConfig;
