a37c5c50cef704ffeabdfaca502735320f53cdbb
Yuta Yamamoto <tuki0918@gmail.com>
2023-11-15 16:54:54 +0900

    Fix Inline-CSS to Tailwind-CSS

--- components/GridRooms.tsx
+++ components/GridRooms.tsx
@@ -6,16 +6,15 @@ export const RoomThumbnail: FC<{ src: string; alt: string }> = ({
   alt,
 }) => {
   return (
-    <Image
-      src={src}
-      width={512}
-      height={512}
-      alt={alt}
-      style={{
-        width: "100%",
-        height: "auto",
-      }}
-    />
+    <div className="w-full">
+      <Image
+        src={src}
+        width={512}
+        height={512}
+        alt={alt}
+        className="w-full h-auto"
+      />
+    </div>
   );
 };
 
@@ -42,13 +41,7 @@ export const GridRooms: FC<{
 }> = ({ contents }) => {
   return (
     <div>
-      <div
-        style={{
-          display: "grid",
-          gridTemplateColumns: "repeat(3, 1fr)",
-          gap: "16px",
-        }}
-      >
+      <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-4">
         {contents.map((room) => (
           <Room key={room.id} src={room.image.url} title={room.title} />
         ))}
