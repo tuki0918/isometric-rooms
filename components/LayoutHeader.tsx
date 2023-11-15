2f19c88f1570a6180867bc13ae38f886ebab1ae0
Yuta Yamamoto <tuki0918@gmail.com>
2023-11-15 23:59:03 +0900

    Impl home button

--- components/LayoutHeader.tsx
+++ components/LayoutHeader.tsx
@@ -1,11 +1,16 @@
 import React, { FC } from "react";
+import Link from "next/link";
 
 export const METADATA_TITLE = "Isometric Rooms";
 export const LayoutHeader: FC = () => {
   return (
     <nav className="bg-gray-800 text-white p-4">
-      <div className="container mx-auto flex items-center justify-between">
-        <div className="font-bold text-lg">{METADATA_TITLE}</div>
+      <div className="container mx-auto flex items-center justify-between min-w-full">
+        <div className="font-bold text-lg">
+          <Link href="/" className="hover:text-gray-300">
+            {METADATA_TITLE}
+          </Link>
+        </div>
       </div>
     </nav>
   );
