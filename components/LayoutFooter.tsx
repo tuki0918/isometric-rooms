640962570da71c6131522583f9c42d2310d20854
Yuta Yamamoto <tuki0918@gmail.com>
2023-11-16 18:05:10 +0900

    Fix Footer compoment

--- components/LayoutFooter.tsx
+++ components/LayoutFooter.tsx
@@ -1,11 +1,19 @@
 import React, { FC } from "react";
+import Image from "next/image";
+import Link from "next/link";
 
-// TODO: footer
 export const LayoutFooter: FC = () => {
   return (
     <div className="bg-gray-800 text-white p-4 mt-8">
-      <div className="container mx-auto text-center">
-        <p>● ● ●</p>
+      <div className="container mx-auto text-center flex items-center justify-center">
+        <Link href="https://github.com/tuki0918/isometric-rooms">
+          <Image
+            src="/github-mark-white.svg"
+            width={32}
+            height={32}
+            alt="Githuv repository"
+          />
+        </Link>
       </div>
     </div>
   );
