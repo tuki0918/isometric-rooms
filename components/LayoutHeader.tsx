d82ac7c482a0931a9d8cd2423a18b29bb660a66f
Yuta Yamamoto <tuki0918@gmail.com>
2023-11-17 20:00:11 +0900

    Add site logo

--- components/LayoutHeader.tsx
+++ components/LayoutHeader.tsx
@@ -1,16 +1,23 @@
 import React, { FC } from "react";
 import Link from "next/link";
+import Image from "next/image";
 import { Outfit } from "next/font/google";
-const outfit = Outfit({ weight: '400', subsets: ['latin'] })
+const outfit = Outfit({ weight: "400", subsets: ["latin"] });
 
-// TODO: Logo
 export const METADATA_TITLE = "Isometric Rooms";
 export const LayoutHeader: FC = () => {
   return (
     <nav className="bg-gray-800 text-white p-4">
       <div className="container mx-auto flex items-center justify-between min-w-full">
         <div className={`font-bold text-lg ${outfit.className}`}>
-          <Link href="/" className="hover:text-gray-300">
+          <Link href="/" className="hover:text-gray-300 flex items-center">
+            <Image
+              src="/logo.png"
+              width={32}
+              height={32}
+              alt="Logo"
+              className="mr-2"
+            />
             {METADATA_TITLE}
           </Link>
         </div>
