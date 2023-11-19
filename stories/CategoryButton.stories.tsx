5c7bdfb9feab5e63114266a375e40c1095bda287
Yuta Yamamoto <tuki0918@gmail.com>
2023-11-19 15:40:46 +0900

    Add CategoryButton stories

---
+++ stories/CategoryButton.stories.tsx
@@ -0,0 +1,41 @@
+import type { Meta, StoryObj } from "@storybook/react";
+import { useState } from "react";
+
+import CategoryButton from "../components/CategoryButton";
+
+const meta: Meta<typeof CategoryButton> = {
+  component: CategoryButton,
+  parameters: {
+    layout: "centered",
+  },
+  tags: ["autodocs"],
+};
+
+export default meta;
+type Story = StoryObj<typeof CategoryButton>;
+
+export const Selected: Story = {
+  args: {
+    category: "すべて",
+    isSelected: true,
+    onSelect: () => void (() => {}),
+  },
+};
+
+export const Clickable: StoryObj<typeof CategoryButton> = {
+  render: (args) => {
+    const [isSelected, setIsSelected] = useState(args.isSelected);
+
+    return (
+      <CategoryButton
+        {...args}
+        isSelected={isSelected}
+        onSelect={() => setIsSelected(!isSelected)}
+      />
+    );
+  },
+  args: {
+    category: "すべて",
+    isSelected: false,
+  },
+};
