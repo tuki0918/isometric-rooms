a1627b73086c883f557fd32f810e193db7fa7901
Yuta Yamamoto <tuki0918@gmail.com>
2023-11-19 16:09:52 +0900

    Add LoadMoreButton stories

---
+++ stories/LoadMoreButton.stories.tsx
@@ -0,0 +1,46 @@
+import type { Meta, StoryObj } from "@storybook/react";
+
+import LoadMoreButton from "../components/LoadMoreButton";
+
+const meta: Meta<typeof LoadMoreButton> = {
+  component: LoadMoreButton,
+  parameters: {
+    layout: "centered",
+  },
+  tags: ["autodocs"],
+};
+
+export default meta;
+type Story = StoryObj<typeof LoadMoreButton>;
+
+export const Default: Story = {
+  args: {
+    isLoading: false,
+    hasMore: false,
+    onLoadMore: () => void (() => {}),
+  },
+};
+
+export const Loading: Story = {
+  args: {
+    isLoading: true,
+    hasMore: true,
+    onLoadMore: () => void (() => {}),
+  },
+};
+
+export const LoadMore: Story = {
+  args: {
+    isLoading: false,
+    hasMore: true,
+    onLoadMore: () => void (() => {}),
+  },
+};
+
+// export const NonExistentCase: Story = {
+//   args: {
+//     isLoading: true,
+//     hasMore: false,
+//     onLoadMore: () => void (() => {}),
+//   },
+// };
