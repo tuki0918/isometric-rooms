3e9af8a1bc2ba992fc70fd28ad0f5b9b907fbcf7
Yuta Yamamoto <tuki0918@gmail.com>
2023-11-20 05:18:29 +0900

    Refactoring hooks/useQueryParams test

--- hooks/useQueryParams.test.ts
+++ hooks/useQueryParams.test.ts
@@ -1,26 +1,48 @@
 import { act, renderHook } from "@testing-library/react";
+import {
+  ReadonlyURLSearchParams,
+  usePathname,
+  useRouter,
+  useSearchParams,
+} from "next/navigation";
 import { useQueryParams } from "./useQueryParams";
 
-const useRouter = jest.fn();
-const usePathname = jest.fn();
-const useSearchParams = jest.fn();
+const mocUseRouter = jest.fn<
+  ReturnType<typeof useRouter>,
+  Parameters<typeof useRouter>
+>();
+const mocUsePathname = jest.fn<
+  ReturnType<typeof usePathname>,
+  Parameters<typeof usePathname>
+>();
+const mockUseSearchParams = jest.fn<
+  ReturnType<typeof useSearchParams>,
+  Parameters<typeof useSearchParams>
+>();
 
 jest.mock("next/navigation", () => ({
-  useRouter: () => useRouter(),
-  usePathname: () => usePathname(),
-  useSearchParams: () => useSearchParams(),
+  useRouter: () => mocUseRouter(),
+  usePathname: () => mocUsePathname(),
+  useSearchParams: () => mockUseSearchParams(),
 }));
 
 describe("useQueryParams", () => {
-  const mockPush = jest.fn();
+  const mockPush = jest.fn<
+    ReturnType<typeof useRouter>["push"],
+    Parameters<ReturnType<typeof useRouter>["push"]>
+  >();
   const mockPathname = "/test";
   const mockSearchParams = new URLSearchParams();
 
   beforeEach(() => {
     jest.clearAllMocks();
-    useRouter.mockReturnValue({ push: mockPush });
-    usePathname.mockReturnValue(mockPathname);
-    useSearchParams.mockReturnValue(mockSearchParams);
+    mocUseRouter.mockReturnValue({ push: mockPush } as unknown as ReturnType<
+      typeof useRouter
+    >);
+    mocUsePathname.mockReturnValue(mockPathname);
+    mockUseSearchParams.mockReturnValue(
+      mockSearchParams as ReadonlyURLSearchParams,
+    );
   });
 
   it("router.push is executed when setQueryParam is set", () => {
