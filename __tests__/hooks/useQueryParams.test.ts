c165e29f6e4d3310a98e8592e902eff10aac7ced
Yuta Yamamoto <tuki0918@gmail.com>
2023-11-19 21:25:52 +0900

    Add hooks/useQueryParams test

---
+++ __tests__/hooks/useQueryParams.test.ts
@@ -0,0 +1,35 @@
+import { act, renderHook } from "@testing-library/react";
+import { useQueryParams } from "../../hooks/useQueryParams";
+
+const useRouter = jest.fn();
+const usePathname = jest.fn();
+const useSearchParams = jest.fn();
+
+jest.mock("next/navigation", () => ({
+  useRouter: () => useRouter(),
+  usePathname: () => usePathname(),
+  useSearchParams: () => useSearchParams(),
+}));
+
+describe("useQueryParams", () => {
+  const mockPush = jest.fn();
+  const mockPathname = "/test";
+  const mockSearchParams = new URLSearchParams();
+
+  beforeEach(() => {
+    jest.clearAllMocks();
+    useRouter.mockReturnValue({ push: mockPush });
+    usePathname.mockReturnValue(mockPathname);
+    useSearchParams.mockReturnValue(mockSearchParams);
+  });
+
+  it("router.push is executed when setQueryParam is set", () => {
+    const { result } = renderHook(() => useQueryParams());
+
+    act(() => {
+      result.current.setQueryParam("key", "value");
+    });
+
+    expect(mockPush.mock.calls[0][0]).toBe("/test?key=value");
+  });
+});
