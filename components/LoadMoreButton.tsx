fd19f957535c37883c62bac2a137b9d74702b2a1
Yuta Yamamoto <tuki0918@gmail.com>
2023-11-21 04:53:02 +0900

    Fix text in LoadMoreButton component

--- components/LoadMoreButton.tsx
+++ components/LoadMoreButton.tsx
@@ -1,23 +1,23 @@
+import { useTranslations } from "next-intl";
 import { FC } from "react";
 
 const LoadMoreButton: FC<{
   isLoading: boolean;
   hasMore: boolean;
   onLoadMore: () => void;
-}> = ({ isLoading, hasMore, onLoadMore }) => (
-  <div className="mt-8 flex justify-center">
-    <button
-      onClick={onLoadMore}
-      disabled={isLoading || !hasMore}
-      className="rounded-full bg-white px-4 py-2 text-xs font-semibold text-blue-500 hover:bg-blue-500 hover:text-white"
-    >
-      {isLoading
-        ? "Loading more..."
-        : hasMore
-          ? "Load More"
-          : "Nothing more to load"}
-    </button>
-  </div>
-);
+}> = ({ isLoading, hasMore, onLoadMore }) => {
+  const t = useTranslations("LoadMoreButton");
+  return (
+    <div className="mt-8 flex justify-center">
+      <button
+        onClick={onLoadMore}
+        disabled={isLoading || !hasMore}
+        className="rounded-full bg-white px-4 py-2 text-xs font-semibold text-blue-500 hover:bg-blue-500 hover:text-white"
+      >
+        {isLoading ? t("isLoading") : hasMore ? t("hasMore") : t("nothing")}
+      </button>
+    </div>
+  );
+};
 
 export default LoadMoreButton;
