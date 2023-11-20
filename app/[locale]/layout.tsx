e48301caf06c43ece50e30ab00de2baa7819c8ee
Yuta Yamamoto <tuki0918@gmail.com>
2023-11-21 01:14:30 +0900

    Fix types (app/[locale]/layout.tsx)

--- app/[locale]/layout.tsx
+++ app/[locale]/layout.tsx
@@ -1,14 +1,15 @@
 import { notFound } from "next/navigation";
+import { i18n, Locale } from "./../../utils/i18n/i18n-config";
 
 // Can be imported from a shared config
-const locales = ["en", "ja"];
+const locales: Locale[] = i18n.locales as unknown as Locale[];
 
 export default function LocaleLayout({
   children,
   params: { locale },
 }: {
   children: React.ReactNode;
-  params: { locale: string };
+  params: { locale: Locale };
 }) {
   // Validate that the incoming `locale` parameter is valid
   if (!locales.includes(locale)) notFound();
