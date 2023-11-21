617921af09a45d41d6a896b3a06b844da54acd73
Yuta Yamamoto <tuki0918@gmail.com>
2023-11-22 04:04:28 +0900

    Fix github logo size

--- components/LayoutFooter.tsx
+++ components/LayoutFooter.tsx
@@ -5,13 +5,14 @@ import { FC } from "react";
 const LayoutFooter: FC = () => {
   return (
     <div className="mt-8 bg-gray-800 p-4 text-white">
-      <div className="container mx-auto flex items-center justify-center text-center">
+      <div className="container mx-auto flex h-8 w-8 items-center justify-center text-center">
         <Link href="https://github.com/tuki0918/isometric-rooms">
           <Image
             src="/github-mark-white.svg"
-            width={32}
-            height={32}
+            width={96}
+            height={96}
             alt="Githuv repository"
+            className="h-auto w-full"
           />
         </Link>
       </div>
