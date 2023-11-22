e319b78cea2d4ad1ffd85951d92c918d247d7fe9
Yuta Yamamoto <tuki0918@gmail.com>
2023-11-23 02:26:34 +0900

    Fix LayoutFooter

--- components/LayoutFooter.tsx
+++ components/LayoutFooter.tsx
@@ -1,20 +1,35 @@
-import Image from "next/image";
-import Link from "next/link";
 import { FC } from "react";
 
 const LayoutFooter: FC = () => {
   return (
-    <div className="mt-16 bg-gray-800 p-4 text-white">
-      <div className="container mx-auto flex h-8 w-8 items-center justify-center text-center">
-        <Link href="https://github.com/tuki0918/isometric-rooms">
-          <Image
-            src="/github-mark-white.svg"
-            width={96}
-            height={96}
-            alt="Githuv repository"
-            className="h-auto w-full"
-          />
-        </Link>
+    <div className="mt-16 flex-col bg-gray-800 p-4 text-white sm:flex sm:flex-row sm:items-center sm:justify-between sm:p-6 xl:p-8">
+      <div className="mb-3 text-center text-sm text-gray-500 sm:my-0">
+        &copy; 20XX-20XX{" "}
+        <a href="#" className="hover:underline" target="_blank">
+          XXX
+        </a>
+        . All rights reserved.
+      </div>
+
+      <div className="text-center">
+        <a
+          href="https://github.com/tuki0918/isometric-rooms"
+          className="inline-block h-6 w-6"
+          target="_blank"
+        >
+          <svg
+            aria-hidden="true"
+            xmlns="http://www.w3.org/2000/svg"
+            fill="currentColor"
+            viewBox="0 0 20 20"
+          >
+            <path
+              fillRule="evenodd"
+              d="M10 .333A9.911 9.911 0 0 0 6.866 19.65c.5.092.678-.215.678-.477 0-.237-.01-1.017-.014-1.845-2.757.6-3.338-1.169-3.338-1.169a2.627 2.627 0 0 0-1.1-1.451c-.9-.615.07-.6.07-.6a2.084 2.084 0 0 1 1.518 1.021 2.11 2.11 0 0 0 2.884.823c.044-.503.268-.973.63-1.325-2.2-.25-4.516-1.1-4.516-4.9A3.832 3.832 0 0 1 4.7 7.068a3.56 3.56 0 0 1 .095-2.623s.832-.266 2.726 1.016a9.409 9.409 0 0 1 4.962 0c1.89-1.282 2.717-1.016 2.717-1.016.366.83.402 1.768.1 2.623a3.827 3.827 0 0 1 1.02 2.659c0 3.807-2.319 4.644-4.525 4.889a2.366 2.366 0 0 1 .673 1.834c0 1.326-.012 2.394-.012 2.72 0 .263.18.572.681.475A9.911 9.911 0 0 0 10 .333Z"
+              clipRule="evenodd"
+            />
+          </svg>
+        </a>
       </div>
     </div>
   );
