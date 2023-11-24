9b528697bf4547142b9b8dcb70740f4d7d75c252
Yuta Yamamoto <tuki0918@gmail.com>
2023-11-25 04:40:44 +0900

    Refactoring app/[locale]/page

--- app/[locale]/page.tsx
+++ app/[locale]/page.tsx
@@ -2,6 +2,7 @@
 
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import Top from "components/Top";
+import { Link } from "utils/i18n/navigation";
 
 const queryClient = new QueryClient();
 
@@ -19,6 +20,29 @@ export default function Home() {
             XXX XXX XXX XXX XXX XXX XXX XXX XXX XXX XXX XXX XXX XXX XXX XXX XXX
             XXX XXX
           </p>
+          <div className="mt-6 flex flex-row justify-center">
+            <Link
+              href={"/information"}
+              className="inline-flex items-center justify-center rounded-lg border border-gray-700 px-5 py-3 text-center text-base font-medium text-white hover:bg-gray-700 focus:ring-4 focus:ring-gray-800 sm:ms-4"
+            >
+              See Information
+              <svg
+                className="ms-2 h-3.5 w-3.5 rtl:rotate-180"
+                aria-hidden="true"
+                xmlns="http://www.w3.org/2000/svg"
+                fill="none"
+                viewBox="0 0 14 10"
+              >
+                <path
+                  stroke="currentColor"
+                  strokeLinecap="round"
+                  strokeLinejoin="round"
+                  strokeWidth="2"
+                  d="M1 5h12m0 0L9 1m4 4L9 9"
+                />
+              </svg>
+            </Link>
+          </div>
         </div>
       </div>
       <QueryClientProvider client={queryClient}>
