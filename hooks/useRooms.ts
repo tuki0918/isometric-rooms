322f896cf2748b50508823e5d616005a51002430
Yuta Yamamoto <tuki0918@gmail.com>
2023-11-24 18:17:58 +0900

    Refactoring fetch functions for microcms

--- hooks/useRooms.ts
+++ hooks/useRooms.ts
@@ -1,6 +1,5 @@
-import { useInfiniteQuery } from "@tanstack/react-query";
-import type { ApiResponse, ContentBase, Image, Queries } from "types/microcms";
-import { client } from "utils/microCMS";
+import type { ContentBase, Image } from "types/microcms";
+import { InfiniteContentsQueries, useInfiniteContents } from "utils/microCMS";
 
 export type ContentCategory = "部屋" | "施設" | "モノ" | "自然" | "未分類";
 
@@ -15,31 +14,6 @@ interface RoomContent extends ContentBase {
   is_generated_by_ai: boolean;
 }
 
-const fetchRooms = async (queries: Queries) => {
-  return await client.get<ApiResponse<RoomContent>>({
-    endpoint: "rooms",
-    queries,
-  });
-};
-
-/**
- * `limit` is required for calculating `offset`.
- */
-export const useInfiniteRooms = (queries: Queries & { limit: number }) => {
-  return useInfiniteQuery({
-    initialPageParam: 0,
-    queryKey: ["rooms", queries],
-    queryFn: ({ pageParam }) => {
-      // Calculate `offset` based on `pageParam`
-      return fetchRooms({ ...queries, offset: queries.limit * pageParam });
-    },
-    getNextPageParam: (lastPage, allPages) => {
-      const { contents, offset, totalCount } = lastPage;
-      // Return `undefined` if all content has already been retrieved.
-      const lastPageCount = offset + contents.length;
-      if (lastPageCount >= totalCount) return undefined;
-      // Return the next `pageParam` if there is unretrieved content.
-      return allPages.length;
-    },
-  });
+export const useInfiniteRooms = (queries: InfiniteContentsQueries) => {
+  return useInfiniteContents<RoomContent>("rooms", queries);
 };
--- utils/microCMS.ts
+++ utils/microCMS.ts
@@ -1,4 +1,8 @@
+import { useInfiniteQuery } from "@tanstack/react-query";
 import { createClient } from "microcms-js-sdk";
+import type { ApiResponse, ContentBase, Queries } from "types/microcms";
+
+export type InfiniteContentsQueries = Queries & { limit: number };
 
 // Initialize the microCMS client (GET only)
 export const client = createClient({
@@ -9,3 +13,39 @@ export const client = createClient({
     process.env.NEXT_PUBLIC_MICROCMS_APIKEY ||
     "NEXT_PUBLIC_MICROCMS_APIKEY is not defined",
 });
+
+export const fetchContents = async <T extends ContentBase>(
+  endpoint: string,
+  queries: Queries,
+) => {
+  return await client.get<ApiResponse<T>>({
+    endpoint,
+    queries,
+  });
+};
+
+/**
+ * `limit` is required for calculating `offset`.
+ */
+export const useInfiniteContents = <T extends ContentBase>(
+  endpoint: string,
+  queries: InfiniteContentsQueries,
+) => {
+  return useInfiniteQuery({
+    initialPageParam: 0,
+    queryKey: [endpoint, queries],
+    queryFn: ({ pageParam }) => {
+      // Calculate `offset` based on `pageParam`
+      const offset = queries.limit * pageParam;
+      return fetchContents<T>(endpoint, { ...queries, offset });
+    },
+    getNextPageParam: (lastPage, allPages) => {
+      const { contents, offset, totalCount } = lastPage;
+      // Return `undefined` if all content has already been retrieved. However, this is not strict.
+      const lastPageCount = offset + contents.length;
+      if (lastPageCount >= totalCount) return undefined;
+      // Return the next `pageParam` if there is unretrieved content.
+      return allPages.length;
+    },
+  });
+};
