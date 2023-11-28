bc13c6efc4ee709a15204f302c3b261d4b8092f2
Yuta Yamamoto <tuki0918@gmail.com>
2023-11-29 06:46:29 +0900

    Refactoring InformationFeeds compoment

---
+++ components/InformationBadges.tsx
@@ -0,0 +1,35 @@
+import { InformationContent } from "hooks/useInformations";
+import { useTranslations } from "next-intl";
+import { FC } from "react";
+
+const CriticalBadge: FC<{ label: string }> = ({ label }) => {
+  return (
+    <span className="inline-flex items-center rounded-md bg-yellow-50 px-2 py-1 text-xs font-medium text-yellow-800 ring-1 ring-inset ring-yellow-600/20">
+      {label}
+    </span>
+  );
+};
+
+const NoticeBadge: FC<{ label: string }> = ({ label }) => {
+  return (
+    <span className="inline-flex items-center rounded-md bg-gray-50 px-2 py-1 text-xs font-medium text-gray-600 ring-1 ring-inset ring-gray-500/10">
+      {label}
+    </span>
+  );
+};
+
+const InformationBadges: FC<{
+  content: InformationContent;
+}> = ({ content }) => {
+  const t = useTranslations("Information");
+  return (
+    <div className="flex justify-start space-x-2">
+      {content.category.map((category) => {
+        return <NoticeBadge key={category} label={t(category)} />;
+      })}
+      {content.is_critical && <CriticalBadge label={t("重要")} />}
+    </div>
+  );
+};
+
+export default InformationBadges;
--- components/InformationFeeds.tsx
+++ components/InformationFeeds.tsx
@@ -1,37 +1,8 @@
 import { InformationContent } from "hooks/useInformations";
-import { useTranslations } from "next-intl";
 import { FC } from "react";
+import { Link } from "utils/i18n/navigation";
 import { formatDate } from "utils/microCMS";
-
-const CriticalBadge: FC<{ label: string }> = ({ label }) => {
-  return (
-    <span className="inline-flex items-center rounded-md bg-yellow-50 px-2 py-1 text-xs font-medium text-yellow-800 ring-1 ring-inset ring-yellow-600/20">
-      {label}
-    </span>
-  );
-};
-
-const NoticeBadge: FC<{ label: string }> = ({ label }) => {
-  return (
-    <span className="inline-flex items-center rounded-md bg-gray-50 px-2 py-1 text-xs font-medium text-gray-600 ring-1 ring-inset ring-gray-500/10">
-      {label}
-    </span>
-  );
-};
-
-const InformationBadges: FC<{
-  content: InformationContent;
-}> = ({ content }) => {
-  const t = useTranslations("Information");
-  return (
-    <div className="flex justify-start space-x-2">
-      {content.category.map((category) => {
-        return <NoticeBadge key={category} label={t(category)} />;
-      })}
-      {content.is_critical && <CriticalBadge label={t("重要")} />}
-    </div>
-  );
-};
+import InformationBadges from "./InformationBadges";
 
 export const SkeletonInformation: FC = () => {
   return (
@@ -68,8 +39,9 @@ const InformationFeeds: FC<{
                 <div className="lg:hidden">{date}</div>
                 <InformationBadges content={content} />
               </div>
-              {/* TODO: link to information detail page */}
-              <div className="font-medium">{content.title}</div>
+              <div className="font-medium">
+                <Link href={`/information/${content.id}`}>{content.title}</Link>
+              </div>
               {content.summary && (
                 <p className=" text-slate-500">{content.summary}</p>
               )}
