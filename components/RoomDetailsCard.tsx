72b96f22a0b9765f979777314e3107d9d2d62b94
Yuta Yamamoto <tuki0918@gmail.com>
2023-12-03 20:48:38 +0900

    Add skeleton for RoomDetailsCard

--- components/RoomDetailsCard.tsx
+++ components/RoomDetailsCard.tsx
@@ -80,4 +80,77 @@ const RoomDetailsCard: FC<{ content: RoomCard }> = ({ content }) => {
   );
 };
 
+export const SkeletonRoomDetailsCard: FC = () => {
+  return (
+    <div>
+      <div className="flex flex-col md:flex-row">
+        <div className="bg-white p-4 shadow md:w-2/3 md:p-8">
+          <div className="flex flex-col items-center md:flex-row">
+            <div className="mb-4 flex aspect-square w-full items-center justify-center rounded bg-gray-300 dark:bg-gray-700">
+              <svg
+                className="h-10 w-10 text-gray-200 dark:text-gray-600"
+                aria-hidden="true"
+                xmlns="http://www.w3.org/2000/svg"
+                fill="currentColor"
+                viewBox="0 0 16 20"
+              >
+                <path d="M14.066 0H7v5a2 2 0 0 1-2 2H0v11a1.97 1.97 0 0 0 1.934 2h12.132A1.97 1.97 0 0 0 16 18V2a1.97 1.97 0 0 0-1.934-2ZM10.5 6a1.5 1.5 0 1 1 0 2.999A1.5 1.5 0 0 1 10.5 6Zm2.221 10.515a1 1 0 0 1-.858.485h-8a1 1 0 0 1-.9-1.43L5.6 10.039a.978.978 0 0 1 .936-.57 1 1 0 0 1 .9.632l1.181 2.981.541-1a.945.945 0 0 1 .883-.522 1 1 0 0 1 .879.529l1.832 3.438a1 1 0 0 1-.031.988Z" />
+                <path d="M5 5V.13a2.96 2.96 0 0 0-1.293.749L.879 3.707A2.98 2.98 0 0 0 .13 5H5Z" />
+              </svg>
+            </div>
+          </div>
+        </div>
+        <div className="bg-white p-4 md:w-1/3 md:p-8 md:pl-0">
+          <div className="mb-4">
+            <h1 className="mb-6 text-center text-2xl font-bold">DATA</h1>
+            <div className="border-t border-gray-300"></div>
+          </div>
+          <div className="grid grid-cols-2 gap-4 text-gray-700 md:grid-cols-1 md:gap-0">
+            <div className="flex flex-col">
+              {/* Left Column */}
+              <div className="mb-2">
+                <div className="bg-gray-100 p-2 font-semibold">
+                  <div className="h-2.5 w-24 rounded-full bg-gray-200 dark:bg-gray-700"></div>
+                </div>
+                <div className="p-2">
+                  <div className="h-2.5 w-24 rounded-full bg-gray-200 dark:bg-gray-700"></div>
+                </div>
+              </div>
+              <div className="mb-2">
+                <div className="bg-gray-100 p-2 font-semibold">
+                  <div className="h-2.5 w-24 rounded-full bg-gray-200 dark:bg-gray-700"></div>
+                </div>
+                <div className="p-2">
+                  <div className="h-2.5 w-24 rounded-full bg-gray-200 dark:bg-gray-700"></div>
+                </div>
+              </div>
+            </div>
+            <div className="flex flex-col">
+              {/* Right Column */}
+              <div className="mb-2">
+                <div className="bg-gray-100 p-2 font-semibold">
+                  <div className="h-2.5 w-24 rounded-full bg-gray-200 dark:bg-gray-700"></div>
+                </div>
+                <div className="p-2">
+                  <div className="h-2.5 w-24 rounded-full bg-gray-200 dark:bg-gray-700"></div>
+                </div>
+              </div>
+              <div className="mb-2">
+                <div className="bg-gray-100 p-2 font-semibold">
+                  <div className="h-2.5 w-24 rounded-full bg-gray-200 dark:bg-gray-700"></div>
+                </div>
+                <div className="p-2">
+                  <div className="flex items-center space-x-2">
+                    <div className="h-2.5 w-24 rounded-full bg-gray-200 dark:bg-gray-700"></div>
+                  </div>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
+
 export default RoomDetailsCard;
