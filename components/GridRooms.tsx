88f72e8bfe2d4681a2e8b7f0043701767af5e176
Yuta Yamamoto <tuki0918@gmail.com>
2023-12-03 15:41:22 +0900

    Impl jump to room content page

--- components/GridRooms.tsx
+++ components/GridRooms.tsx
@@ -2,6 +2,7 @@ import { RoomCard } from "domains/RoomCard";
 import { useTranslations } from "next-intl";
 import Image from "next/image";
 import { FC, useState } from "react";
+import { Link } from "utils/i18n/navigation";
 
 const RoomThumbnail: FC<{
   src: string;
@@ -22,24 +23,17 @@ const RoomThumbnail: FC<{
   );
 };
 
-const DownloadButton: FC<{
-  src: string;
-}> = ({ src }) => {
+const OpenButton: FC<{
+  content: RoomCard;
+}> = ({ content }) => {
+  const t = useTranslations("GridRooms");
   return (
-    <a
-      href={src}
-      className="inline-flex items-center rounded border-4 px-4 py-2 text-lg font-bold text-white hover:border-emerald-500 hover:text-emerald-500"
-      target="_blank"
+    <Link
+      href={`/room/${content.room.id}`}
+      className="inline-flex items-center rounded border-4 px-8 py-2 text-lg font-bold text-white hover:border-cyan-500 hover:text-cyan-500"
     >
-      <svg
-        className="mr-2 h-4 w-4 fill-current"
-        xmlns="http://www.w3.org/2000/svg"
-        viewBox="0 0 20 20"
-      >
-        <path d="M13 8V2H7v6H2l8 8 8-8h-5zM0 18h20v2H0v-2z" />
-      </svg>
-      <span>Download</span>
-    </a>
+      <span>{t("open")}</span>
+    </Link>
   );
 };
 
@@ -67,7 +61,7 @@ export const GridRoomCard: FC<{
       {hover && (
         <div className="absolute inset-0 flex items-center justify-center bg-black bg-opacity-50">
           <div className="flex items-center justify-center">
-            <DownloadButton src={content.room.image.url} />
+            <OpenButton content={content} />
           </div>
         </div>
       )}
