b64c0f943953b246fc76ca1466b7538b362b42db
Yuta Yamamoto <tuki0918@gmail.com>
2023-12-05 03:57:16 +0900

    Add GPT link to top page

--- app/[locale]/page.tsx
+++ app/[locale]/page.tsx
@@ -37,12 +37,35 @@ export default function Home() {
             XXX XXX XXX XXX XXX XXX XXX XXX XXX XXX XXX XXX XXX XXX XXX XXX XXX
             XXX XXX
           </p>
-          <div className="mt-6 flex flex-row justify-center">
+          <div className="mt-6 inline-flex flex-col space-y-4 md:flex-row md:justify-center md:space-y-0">
             <Link
               href={"/information"}
               className="inline-flex items-center justify-center rounded-lg border border-gray-700 px-5 py-3 text-center text-base font-medium text-white hover:bg-gray-700 focus:ring-4 focus:ring-gray-800 sm:ms-4"
             >
               See Information
+              <svg
+                className="ms-2 h-3.5 w-3.5 rtl:rotate-180"
+                aria-hidden="true"
+                xmlns="http://www.w3.org/2000/svg"
+                fill="none"
+                viewBox="0 0 14 10"
+              >
+                <path
+                  stroke="currentColor"
+                  strokeLinecap="round"
+                  strokeLinejoin="round"
+                  strokeWidth="2"
+                  d="M1 5h12m0 0L9 1m4 4L9 9"
+                />
+              </svg>
+            </Link>
+            <Link
+              href={
+                "https://chat.openai.com/g/g-OvSZ5XwWK-isometric-room-vision"
+              }
+              className="inline-flex items-center justify-center rounded-lg border border-gray-700 px-5 py-3 text-center text-base font-medium text-white hover:bg-gray-700 focus:ring-4 focus:ring-gray-800 sm:ms-4"
+            >
+              See Image Generation GPT
               <svg
                 className="ms-2 h-3.5 w-3.5 rtl:rotate-180"
                 aria-hidden="true"
